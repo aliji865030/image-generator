d7b26d5c76430a6e25045b58336b889c2128397d
aliji865030 <abbasalichand786@gmail.com>
2024-03-22 00:57:29 +0530

    some modification

--- src/screen/LandingScreen.jsx
+++ src/screen/LandingScreen.jsx
@@ -1,66 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from "axios"
 
 const LandingScreen = () => {
+    // let data=[]
+    const [data,setData]=useState([]);
+    const [page,setPage]=useState(1);
+    const [query,setQuery]=useState("home")
+
+    const api_key="_fQKKieztygyfJM4s4OWJROZnYHTfbH5xrkSIpU0cIU"
+    const api_url=`https://api.unsplash.com/search/photos?page=${page}&query=${query}`
+
+
+
+
+ useEffect(()=>{
 
     
-//    window.onload=async function get(){
-//         // const axios = require('axios');
-
-// const options = {
-//   method: 'GET',
-//   url: 'https://img4me.p.rapidapi.com/',
-//   params: {
-//     text: 'Home',
-//     font: 'trebuchet',
-//     size: '12',
-//     fcolor: '000000',
-//     bcolor: 'FFFFFF',
-//     type: 'png'
-//   },
-//   headers: {
-//     'X-RapidAPI-Key': 'd3ca8691b5mshdf20109d6f12293p1f90b5jsnc64cef66f8f5',
-//     'X-RapidAPI-Host': 'img4me.p.rapidapi.com'
-//   }
-// };
-
-// try {
-// 	const response = await axios.request(options);
-// 	console.log(response.data);
-// } catch (error) {
-// 	console.error(error);
-// }
-//     }
-
-
-// window.onload=function(){
-//     const API_TOKEN="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"
-//     async function query(data) {
-//         const response = await fetch(
-//             "https://api-inference.huggingface.co/models/gpt2",
-//             {
-//                 headers: { Authorization: `Bearer ${API_TOKEN}` },
-//                 method: "POST",
-//                 body: JSON.stringify(data),
-//             }
-//         );
-//         const result = await response.json();
-//         return result;
-//     }
-//     query("home").then((response) => {
-//         console.log(response);
-//     });
-// }
-
-
-
-
-window.onload=function(){
-    const prompt="home"
-    fetch(`https://craiyon.AjaySinghUsesGi.repl.co/api?prompt=${prompt}`)
-    .then(response => response.json())
-    .then((parsedResponse)=>{console.log(parsedResponse)})
-}
+        fetch(`${api_url}&per_page=30&client_id=${api_key}`)
+        .then(response => response.json())
+        .then((parsedResponse)=>{
+            // data=[]
+            setData([])
+            for(let i=0;i<parsedResponse.results.length;i++){
+                // data.push(parsedResponse.results[i].urls.full);
+                setData((preData)=>[...preData,parsedResponse.results[i].urls.regular])
+                // setData((preData)=>[...preData,tempData])
+                // console.log(i);
+            }
+            console.log(parsedResponse.results[1])
+            
+            // console.log(data);
+            
+            // console.log(parsedResponse.results[1].urls.full)
+    
+        })
+    
+    
+    
+ },[page],[]) 
 
     
 
@@ -71,10 +48,32 @@ window.onload=function(){
 
   return (
     <div>
+        <div><h1 className='text-3xl font-bold py-10'>An Image Search APP</h1></div>
+          
+        <div className='flex items-center justify-center gap-80  pb-10'>
+        <div>
+        <button className='border rounded-md bg-green-500 hover:bg-green-600 text-white px-5 py-1' onClick={()=>{
+            setPage((prePage)=>prePage+1)
+            // getData();
+          }}>Load more</button>
+        </div>
+            <div className='flex gap-5'>
+            <input  className='border-2 rounded-md px-5 py-2' type="text" placeholder='Search Image' onChange={(e)=>setQuery(e.target.value)} />
+            <button className='border rounded-md bg-blue-500 hover:bg-blue-600 text-white px-5 py-1'  onClick={()=>{setPage(prePage=>prePage+1)}}>Show result</button>
+            </div>
+        </div>
 
-      
+      <div className='flex flex-wrap gap-10 justify-center'>
+      {data.map((item,index)=>{
+        return(
+            <div key={index}>
+                <img className='border-4 rounded-md  ' style={{height:"300px", width:"300px"}} src={item} alt="" />
+            </div>
+        )
+      })}
+      </div>
     </div>
   )
 }
 
-export default LandingScreen
+export default LandingScreen;
